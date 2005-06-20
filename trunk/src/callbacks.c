f46333b7f613b27c6365d1e4cde7a0b1f0ce9df4
berli <berli@6a1ef0c4-92f9-0310-931d-bf668151e6df>
2005-06-20 00:31:13 +0000

    export complete glyphs, even pixels outside their bounding box

--- trunk/src/callbacks.c
+++ trunk/src/callbacks.c
@@ -39,6 +39,73 @@ on_filechooserdialog1_delete_event     (GtkWidget       *widget,
 	return TRUE;
 }
 
+void
+get_real_pixel_size(GtkWidget *widget, PangoLayout *layout, int *l, int *t, int *r, int *b)
+{
+	int w, h;
+	pango_layout_get_pixel_size(layout, &w, &h);
+	w *= 3;
+	h *= 3;
+	GdkPixmap *pixmap = gdk_pixmap_new(widget->window, w, h, -1);
+
+	GdkGC *gc = gdk_gc_new(pixmap);
+	GdkColor color_transparent = {0, 65535, 65535, 65535};
+	GdkColor color_font = {0, 0, 0, 0};
+
+	gdk_gc_set_rgb_fg_color(gc, &color_transparent);
+	gdk_draw_rectangle(pixmap, gc, TRUE, 0, 0, w, h);
+
+	gdk_gc_set_rgb_fg_color(gc, &color_font);
+	gdk_draw_layout(pixmap, gc, w / 3, h / 3, layout);
+
+	GdkPixbuf *pixbuf = gdk_pixbuf_get_from_drawable(NULL,
+		pixmap, NULL, 0, 0, 0, 0, w, h);
+	guchar *pixels = gdk_pixbuf_get_pixels(pixbuf);
+
+	int rowstride = gdk_pixbuf_get_rowstride (pixbuf);
+
+	*l = w;
+	*t = h;
+	*r = -1;
+	*b = -1;
+	int x, y;
+	for (y = 0; y < h; y++)
+	{
+		for (x = 0; x < w; x++)
+		{
+			guchar red = pixels[rowstride * y + x * 3];
+			guchar green = pixels[rowstride * y + x * 3 + 1];
+			guchar blue = pixels[rowstride * y + x * 3 + 2];
+			if (red == 255 && green == 255 && blue == 255)
+				continue;
+			if (x < *l)
+				*l = x;
+			if (y < *t)
+				*t = y;
+			if (x > *r)
+				*r = x;
+			if (y > *b)
+				*b = y;
+		}
+	}
+
+	g_object_unref(pixbuf);
+	g_object_unref(gc);
+	g_object_unref(pixmap);
+
+	if (*r >= 0)
+	{
+		*l -= w / 3;
+		*t -= h / 3;
+		*r -= w / 3;
+		*b -= h / 3;
+	}
+	else
+	{
+		*l = *t = *r = *b = 0;
+	}
+}
+
 
 void
 on_button5_clicked                     (GtkButton       *button,
@@ -61,12 +128,15 @@ on_button5_clicked                     (GtkButton       *button,
 
 	fontname = gtk_font_selection_get_font_name (GTK_FONT_SELECTION (fontsel));
 
+	printf("Exporting font %s.\n", fontname);
+
 	PangoLayout *layout = gtk_widget_create_pango_layout(imagewidget, "?");
 
 	PangoFontDescription *desc = pango_font_description_from_string(fontname);
 
 	pango_layout_set_font_description(layout, desc);
 
+	int shift_down = 0;
 	int gridw = 1, gridh = 1;
 	int i;
 	for (i = first_char; i <= last_char; i++)
@@ -76,17 +146,41 @@ on_button5_clicked                     (GtkButton       *button,
 
 		int w, h;
 		pango_layout_get_pixel_size(layout, &w, &h);
+		/* can't trust the above function, unfortunately */
+
+		int l, t, r, b, rw, rh;
+                get_real_pixel_size(imagewidget, layout, &l, &t, &r, &b);
+		rw = 1 + r;
+		rh = 1 + b;
+		if (l < 0)
+			rw -= l;
+		if (t < 0)
+			rh -= t;
+
+		if (rw > w)
+			w = rw;
+		if (rh > h)
+			h = rh;
 
 		if (w > gridw)
 			gridw = w;
 		if (h > gridh)
 			gridh = h;
+
+		if (t < shift_down)
+			shift_down = t;
 	}
 
 	int tga_w = (gridw + 1) * 16 + 1;
 	int tga_h = (1 + (last_char - first_char) / 16) * (gridh + 1) + 1;
 	int tga_x = 1;
 	int tga_y = 1;
+
+	if (shift_down >= 0)
+		shift_down = 0;
+
+	if (shift_down)
+		printf("Shifting all characters down by %d.\n", shift_down);
 
 	GdkPixmap *pixmap = gdk_pixmap_new(imagewidget->window, tga_w, tga_h, -1);
 
@@ -100,12 +194,35 @@ on_button5_clicked                     (GtkButton       *button,
 
 	for (i = first_char; i <= last_char; i++)
 	{
-		gchar utf8[10];
+		gchar utf8[10] = {0};
 		pango_layout_set_text(layout, utf8, g_unichar_to_utf8(i, utf8));
 
 		int w, h;
 		pango_layout_get_pixel_size(layout, &w, &h);
-                h = gridh;
+
+		int l, t, r, b;
+                get_real_pixel_size(imagewidget, layout, &l, &t, &r, &b);
+
+		if (l >= 0)
+			l = 0;
+
+		r = 1 + r;
+		if (r <= w)
+			r = w;
+
+		if (t >= 0)
+			t = 0;
+
+		b = 1 + b;
+		if (b <= h)
+			b = h;
+
+		if (l || t || r != w || b != h)
+		{
+			printf("Char %d (%s): Oversize by %d %d %d %d pixel!\n", i, utf8, -l, -t, r - w, b - h);
+		}
+		w = r - l;
+		h = gridh;
 
 		GdkRectangle rect = {tga_x, tga_y, w, h};
 		gdk_gc_set_clip_rectangle(gc, &rect);
@@ -114,7 +231,8 @@ on_button5_clicked                     (GtkButton       *button,
 		gdk_draw_rectangle(pixmap, gc, TRUE, tga_x, tga_y, w, h);
 
 		gdk_gc_set_rgb_fg_color(gc, &color_font);
-		gdk_draw_layout(pixmap, gc, tga_x, tga_y, layout);
+
+		gdk_draw_layout(pixmap, gc, tga_x - l, tga_y + shift_down + t, layout);
 
 		tga_x += gridw + 1;
 		if (tga_x >= tga_w)
